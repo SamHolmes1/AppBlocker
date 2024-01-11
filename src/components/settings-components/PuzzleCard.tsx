85d1744bdd93acd9ff53bebf851059a5184b5b02
Chris Gladney <christophergladney@hotmail.co.uk>
2024-01-11 10:55:39 +0000

    Updated settings so they work as expected

--- src/components/settings-components/PuzzleCard.tsx
+++ src/components/settings-components/PuzzleCard.tsx
@@ -1,10 +1,12 @@
 import { useContext, useState, useEffect } from "react";
 import { SettingsContext } from "../../App";
+import { ConfirmationContext } from "../Settings";
 
 const PuzzleCard = () => {
   const gamesArray = ["quiz", "math", "maze"];
   //@ts-ignore
   const { settingsState, setSettingsState } = useContext(SettingsContext);
+  const { canConfirm, setCanConfirm } = useContext(ConfirmationContext);
   // const [checkboxProperties, setCheckboxProperties] = useState("");
 
   useEffect(() => {}, [settingsState]);
@@ -15,30 +17,34 @@ const PuzzleCard = () => {
       settingsBeforeOnChange[gameKey] = !settingsBeforeOnChange[gameKey];
       return settingsBeforeOnChange;
     });
+    if (Object.values(settingsState).includes(true)) {
+      setCanConfirm(true);
+    } else {
+      setCanConfirm(false);
+    }
   };
 
   return (
     <>
       {/* <h3 className="puzzle-card">Puzzle detail here</h3> */}
       <div className="games-checklist">
-      {gamesArray.map((game) => {
-        return (
-          <div key={game} className="individual-quiz-title">
-            <input
-              id="check-boxes"
-              type="checkbox"
-              value={`${game}_selected`}
-              name={game}
-              defaultChecked={settingsState[`${game}_selected`]}
-              onChange={(event) => {
-                handleOnChange(event);
-              }}
-            />
-            <label htmlFor={game}>{game}</label>
-          </div>
-          
-        );
-      })}
+        {gamesArray.map((game) => {
+          return (
+            <div key={game} className="individual-quiz-title">
+              <input
+                id="check-boxes"
+                type="checkbox"
+                value={`${game}_selected`}
+                name={game}
+                defaultChecked={settingsState[`${game}_selected`]}
+                onChange={(event) => {
+                  handleOnChange(event);
+                }}
+              />
+              <label htmlFor={game}>{game}</label>
+            </div>
+          );
+        })}
       </div>
     </>
   );
